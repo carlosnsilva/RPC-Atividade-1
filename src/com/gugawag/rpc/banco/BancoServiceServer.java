9d46fdf00f586ccf99e311ab64902b860d2f31df
carlosnsilva <cmanoel17@gmail.com>
2021-11-07 15:32:06 -0300

    First Commit

---
+++ src/com/gugawag/rpc/banco/BancoServiceServer.java
@@ -0,0 +1,42 @@
+package com.gugawag.rpc.banco;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BancoServiceServer extends UnicastRemoteObject implements BancoServiceIF {
+
+    private Map<String, Double> saldoContas;
+    private List<Conta> contas = new ArrayList<>();;
+
+
+    public BancoServiceServer() throws RemoteException {
+        saldoContas = new HashMap<>();
+        saldoContas.put("1", 100.0);
+        saldoContas.put("2", 156.0);
+        saldoContas.put("3", 950.0);
+        Conta contaTeste = new Conta("1",200.00);
+        contas.add(contaTeste);
+
+    }
+
+    @Override
+    public double saldo(String conta) throws RemoteException {
+        return saldoContas.get(conta);
+    }
+
+    @Override
+    public int quantidadeContas() throws RemoteException {
+        return saldoContas.size();
+    }
+
+    @Override
+    public void adicionarConta(String numero, double saldo) throws RemoteException {
+        Conta contaCriada = new Conta(numero,saldo);
+        contas.add(contaCriada);
+    }
+
+}
